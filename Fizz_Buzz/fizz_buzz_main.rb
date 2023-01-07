1ab31e2c429112d56a6008ee1774408b7ad23a9a
Jorge Rodriguez <jarf4321@gmail.com>
2023-01-06 18:59:00 -0600

    Add-"stage 2" of the "FizzBuzz" coding kata

--- Fizz_Buzz/fizz_buzz_main.rb
+++ Fizz_Buzz/fizz_buzz_main.rb
@@ -4,21 +4,26 @@
 class FizzBuzz
   attr_accessor :start_at, :end_at
 
-  attr_reader :arr_of_result
+  attr_reader :arr_of_result_one, :arr_of_result_two
 
   def initialize(begin_number, end_number)
     @start_at = begin_number
     @end_at = end_number
-    @arr_of_result = fill_up_array_of_results
+    @arr_of_result_one = stage_one
+    @arr_of_result_two = stage_two
   end
 
-  def print
-    p arr_of_result
+  def print_one
+    p arr_of_result_one
+  end
+
+  def print_two
+    p arr_of_result_two
   end
 
   private
 
-  def calculate(number)
+  def calculate_stage_one(number)
     return 'FizzBuzz' if (number % 3).zero? && (number % 5).zero?
 
     return 'Fizz' if (number % 3).zero?
@@ -28,18 +33,41 @@ class FizzBuzz
     number
   end
 
-  def fill_up_array_of_results
+  def stage_one
     results = []
     (start_at..end_at).each do |number|
-      results << calculate(number)
+      results << calculate_stage_one(number)
+    end
+    results
+  end
+
+  def calculate_stage_two(number)
+    result = ''
+
+    result += 'Fizz' if (number % 3).zero?
+    result += 'Fizz' if number.to_s.include?('3')
+    result += 'Buzz' if (number % 5).zero?
+    result += 'Buzz' if number.to_s.include?('5')
+
+    return result unless result.empty?
+
+    number
+  end
+
+  def stage_two
+    results = []
+    (start_at..end_at).each do |number|
+      results << calculate_stage_two(number)
     end
     results
   end
 end
 
 one_to_hundred = FizzBuzz.new(1, 100)
-one_to_hundred.print
+one_to_hundred.print_one
 puts
-puts "FizzBuzz: #{one_to_hundred.arr_of_result.count('FizzBuzz')}"
-puts "Fizz: #{one_to_hundred.arr_of_result.count('Fizz')}"
-puts "Buzz: #{one_to_hundred.arr_of_result.count('Buzz')}"
+one_to_hundred.print_two
+# puts
+# puts "FizzBuzz: #{one_to_hundred.arr_of_result_one.count('FizzBuzz')}"
+# puts "Fizz: #{one_to_hundred.arr_of_result_one.count('Fizz')}"
+# puts "Buzz: #{one_to_hundred.arr_of_result_one.count('Buzz')}"
